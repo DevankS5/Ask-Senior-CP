05cf4bd3c8b654e51c6eb6366a6f54a1d491666d
DevankS5 <Devank.24SCSE1410185@galgotiasuniversity.ac.in>
2025-12-31 00:03:14 +0530

    Create 18. Palindromearray.cpp

---
+++ Basics/18. Palindromearray.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    long long arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < n/2; i++){
+        if(arr[i] != arr[n - i - 1]){
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    cout << "YES" << endl;
+}
