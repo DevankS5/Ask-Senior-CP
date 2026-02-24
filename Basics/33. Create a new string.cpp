fe557aa829c96c06558fb4180a8cf6af6296a1c3
DevankS5 <Devank.24SCSE1410185@galgotiasuniversity.ac.in>
2026-02-24 11:00:59 +0530

    Create 33. Create a new string.cpp

---
+++ Basics/33. Create a new string.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s, n;
+    cin >> s >> n;
+    int length = s.length();
+    cout << length <<" ";
+    int l = n.length();
+    cout << l << endl;
+    cout << s << " " << n ;
+    
+}
