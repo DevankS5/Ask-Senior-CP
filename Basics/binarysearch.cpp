b3322923a51aca2ca3a8c1746647b7a17119e1bf
DevankS5 <Devank.24SCSE1410185@galgotiasuniversity.ac.in>
2026-02-24 11:01:05 +0530

    Create binarysearch.cpp

---
+++ Basics/binarysearch.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binarySearch(int arr[], int l, int r, int x) {
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+        return binarySearch(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {2, 4, 6, 8, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 6;
+    int result = binarySearch(arr, 0, n - 1, x);
+    if (result == -1)
+        cout << "Element not found in array";
+    else
+        cout << "Element found at index: " << result;
+    return 0;
+}
