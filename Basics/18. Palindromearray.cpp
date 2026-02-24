656ff87bd5d0ec2c1526b1bbeb0856fe47dbe848
DevankS5 <Devank.24SCSE1410185@galgotiasuniversity.ac.in>
2026-02-24 11:00:55 +0530

    Update 18. Palindromearray.cpp

--- Basics/18. Palindromearray.cpp
+++ Basics/18. Palindromearray.cpp
@@ -1,17 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin >> n;
-    long long arr[n];
-    for(int i = 0; i < n; i++){
-        cin >> arr[i];
-    }
-    for(int i = 0; i < n/2; i++){
-        if(arr[i] != arr[n - i - 1]){
-            cout << "NO" << endl;
-            return 0;
-        }
-    }
-    cout << "YES" << endl;
-}
+
