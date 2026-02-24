7362efb375bc3f2581a9c63d2d4f26292e391abf
DevankS5 <Devank.24SCSE1410185@galgotiasuniversity.ac.in>
2026-02-24 11:01:14 +0530

    Create transpose.cpp

---
+++ Basics/transpose.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int a[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    int t[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            t[i][j]=a[j][i];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<t[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
